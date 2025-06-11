8af87951eba138dca09ff3676be345f13cfd13aa
Paolo <u20221f613@upc.edu.pe>
2025-06-10 19:25:34 -0500

    initial commit.

---
+++ appweb_back/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Verify Database Connection String
+if (connectionString is null)
+    // Stop the application if the connection string is not set.
+    throw new Exception("Database connection string is not set.");
+
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
