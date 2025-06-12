d3e02bc6430b7745273f5b93662de7c9e0eee581
maria-mm0907 <U202317442@upc.edu.pe>
2025-06-12 01:11:18 -0500

    fix(profiles): add to shared bounded context

--- appweb_back/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ appweb_back/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+    
+namespace appweb_back.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        base.OnConfiguring(builder);
+        // Enable Audit Fields Interceptors
+        builder.AddCreatedUpdatedInterceptor();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Publishing Context
+        
+       
+        // Apply SnakeCase Naming Convention
+       
+    }
+}
