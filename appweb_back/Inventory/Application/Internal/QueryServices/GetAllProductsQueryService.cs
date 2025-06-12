204dc9b3ff61014921d97c1dad3fc82eae32b40e
Paolo <u20221f613@upc.edu.pe>
2025-06-11 22:17:20 -0500

    feat(application): add command and query service for product aggregate.

---
+++ appweb_back/Inventory/Application/Internal/QueryServices/GetAllProductsQueryService.cs
@@ -0,0 +1,20 @@
+﻿using appweb_back.Inventory.Domain.Model.Entities;
+using appweb_back.Inventory.Domain.Model.Queries;
+using appweb_back.Inventory.Domain.Repositories;
+
+namespace appweb_back.Inventory.Application.Internal.QueryServices;
+
+public class GetAllProductsQueryService
+{
+    private readonly IProductRepository _repository;
+
+    public GetAllProductsQueryService(IProductRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<Product>> HandleAsync(GetAllProductsQuery query)
+    {
+        return await _repository.ListAsync();
+    }
+}
