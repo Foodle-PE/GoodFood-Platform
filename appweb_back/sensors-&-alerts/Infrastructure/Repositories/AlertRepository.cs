abef970d33e1e3add8841b1d14fda9e6b8ec3c5b
Luis Angel <1luis4mon@gmail.com>
2025-06-12 10:59:33 -0500

    AlertController. Program.cs

---
+++ appweb_back/sensors-&-alerts/Infrastructure/Repositories/AlertRepository.cs
@@ -0,0 +1,27 @@
+﻿using appweb_back.sensors___alerts.Domain.Model.Entities;
+using appweb_back.sensors___alerts.Domain.Repositories;
+using appweb_back.sensors___alerts.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace appweb_back.sensors___alerts.Infrastructure.Repositories;
+
+public class AlertRepository : IAlertRepository
+{
+    private readonly AppDbContext _context;
+
+    public AlertRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Alert>> GetAllAsync()
+    {
+        return await _context.Alerts.ToListAsync();
+    }
+
+    public async Task AddAsync(Alert alert)
+    {
+        _context.Alerts.Add(alert);
+        await _context.SaveChangesAsync();
+    }
+}
