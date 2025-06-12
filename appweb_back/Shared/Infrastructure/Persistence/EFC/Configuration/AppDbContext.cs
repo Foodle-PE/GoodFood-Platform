de7be8628da29abbb693e95409f9fdbad4a8ddcd
Paolo <u20221f613@upc.edu.pe>
2025-06-11 22:19:00 -0500

    feat(shared): add shared files.

---
+++ appweb_back/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using appweb_back.Inventory.Domain.Model.Aggregates;
+using Microsoft.EntityFrameworkCore;
+
+namespace appweb_back.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().OwnsOne(p => p.Quantity);
+        modelBuilder.Entity<Product>().OwnsOne(p => p.ExpirationDate);
+    }
+}
