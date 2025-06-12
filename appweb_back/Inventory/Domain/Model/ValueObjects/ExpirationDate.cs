d524953f185f6200e653633ca85c981bbedab982
Paolo <u20221f613@upc.edu.pe>
2025-06-11 22:11:47 -0500

    feat(value-objects): add value objects.

---
+++ appweb_back/Inventory/Domain/Model/ValueObjects/ExpirationDate.cs
@@ -0,0 +1,11 @@
+﻿namespace appweb_back.Inventory.Domain.Model.ValueObjects;
+
+public record ExpirationDate(DateTime Value)
+{
+    public static ExpirationDate Create(DateTime value)
+    {
+        if (value <= DateTime.UtcNow)
+            throw new ArgumentException("Expiration date must be in the future.");
+        return new ExpirationDate(value);
+    }
+}
