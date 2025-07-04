40979cb30e1bb63fc0ff98ffcb8be2fb998a8a69
Luiso-AM <140453696+Luiso-AM@users.noreply.github.com>
2025-07-04 16:39:59 -0500

    Update Program.cs

--- appweb_back/Program.cs
+++ appweb_back/Program.cs
@@ -1,30 +1,58 @@
+
+using appweb_back.sensors___alerts.Application.Internal.CommandServices;
+using appweb_back.sensors___alerts.Application.Internal.QueryServices;
+using appweb_back.sensors___alerts.Domain.Repositories;
+using appweb_back.sensors___alerts.Infrastructure.Data;
+using appweb_back.sensors___alerts.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
-
-// Add Database Connection
+// Configuración de MySQL
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySQL(connectionString)); 
 
-// Verify Database Connection String
-if (connectionString is null)
-    // Stop the application if the connection string is not set.
-    throw new Exception("Database connection string is not set.");
 
+//    Add CORS policy 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllPolicy",
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+// Inyección de dependencias
+builder.Services.AddScoped<IAlertRepository, AlertRepository>();
+builder.Services.AddScoped<CreateAlertService>();
+builder.Services.AddScoped<GetAlertsService>();
+
+// Swagger
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Verify if the database exists and create it if it doesn't
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
 
-app.UseHttpsRedirection();
+    context.Database.EnsureCreated();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Apply CORS Policy  
+app.UseCors("AllowAllPolicy");
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
