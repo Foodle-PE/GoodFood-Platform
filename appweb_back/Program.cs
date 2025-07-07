c872c041a3dd1c910001dc99dff26f03b75c1a6b
PaleToFo <u20221f613@upc.edu.pe>
2025-07-07 01:09:09 -0500

    chore: add deployed frontend to cors

--- appweb_back/Program.cs
+++ appweb_back/Program.cs
@@ -48,8 +48,11 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 // Add CORS Policy
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowedAllPolicy", policy =>
-        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+    options.AddPolicy("AllowedFrontendOnly", policy =>
+        policy.WithOrigins("https://goodfood-pe.netlify.app")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials());
 });
 
 
@@ -191,7 +194,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseRouting();
 
-app.UseCors("AllowedAllPolicy");
+app.UseCors("AllowedFrontendOnly");
 
 app.UseHttpsRedirection();
 
