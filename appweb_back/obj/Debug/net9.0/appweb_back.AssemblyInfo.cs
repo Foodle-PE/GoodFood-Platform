f5495f33e940ccfdccfaae019aba3d5e324c050a
Paolo <u20221f613@upc.edu.pe>
2025-07-07 18:41:49 -0500

    testing again, im tired

--- appweb_back/obj/Debug/net9.0/appweb_back.AssemblyInfo.cs
+++ appweb_back/obj/Debug/net9.0/appweb_back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appweb_back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66713e9b4631b9071fb79a08edd51cc7b82f341d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8294a41d8cf1f4406f27ebbe88303d8b281b12a")]
 [assembly: System.Reflection.AssemblyProductAttribute("appweb_back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appweb_back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
